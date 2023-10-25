294257384e4bdcdb249d02c43523e79614626b4b
Huy <trinhdamhuy@gmail.com>
2023-10-25 13:00:45 +0700

    chỉnh card complete

--- components/Completed.tsx
+++ components/Completed.tsx
@@ -7,19 +7,17 @@ const Completed = () => {
       <h1 className="text-4xl font-bold text-center my-5 pt-10">
         Completed Projects
       </h1>
-      <hr className="max-w-4xl h-1 mx-auto my-4 bg-green-300 border-0 rounded md:my-10 dark:bg-gray-700"></hr>
+      <hr className="max-w-4xl h-0.5 mx-auto my-4 bg-xanh-le border-0 rounded md:my-10"></hr>
       <div className="flex flex-row gap-2 mx-3">
         <div className="max-w-3xl w-full">
           <CardCompleted />
         </div>
 
-        <div className="shadow-lg border border-gray-200 bg-emerald-400 rounded-lg mt-5 my-3  w-full max-w-lg">
-          <span className="text-xl font-bold ps-5 pt-5">More</span>
-          <div className="mx-5 my-5 flex flex-col gap-4">
-            <CardMore />
-            <CardMore />
-            <CardMore />
-          </div>
+        <div className="flex flex-col gap-4 shadow-lg border border-gray-200 bg-xanh-dam rounded-lg mt-5 my-3 p-5 w-full max-w-lg">
+          <span className="text-xl font-bold text-white">More</span>
+          <CardMore />
+          <CardMore />
+          <CardMore />
         </div>
       </div>
     </div>
