e1f525b0f238e82855951b14f9fb7edfadf1772f
ThxgHntr <121294842+ThxgHntr@users.noreply.github.com>
2023-10-25 11:56:10 +0000

    con cac

--- components/CardCompleted.tsx
+++ components/CardCompleted.tsx
@@ -3,48 +3,46 @@ import { CiShare1 } from "react-icons/ci";
 
 const CardCompleted = () => {
   return (
-    <div className="h-full shadow-lg border border-gray-200 bg-white rounded-lg">
-      <div className="flex flex-col gap-2">
-        <div className="p-3">
-          <p className="text-2xl font-bold">Project name</p>
-          <div className="flex flex-row items-center justify-end gap-2">
-            <IoCalendarOutline className="w-4 h-4" />
-            <span>10/2023-10/2024</span>
-          </div>
+    <div className="flex flex-col gap-2">
+      <div className="p-3">
+        <p className="text-2xl font-bold">Project name</p>
+        <div className="flex flex-row items-center justify-end gap-2">
+          <IoCalendarOutline className="w-5 h-5" />
+          <span>10/2023-10/2024</span>
         </div>
-        <iframe
-          width="auto"
-          height="400"
-          src="https://www.youtube.com/embed/543jwFfCZvg?si=qPcHutduGIYha_68"
-          title="YouTube video player"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        ></iframe>
-        <div className="flex flex-col content-start w-auto p-5 gap-3">
-          <div>
-            <p className="text-base">
-              Description shdgajhd dáhjgda ádjgdjahs djasgdasjbd jasgd
-              ádjasgdajs duiassgdasj
-            </p>
+      </div>
+      <iframe
+        width="auto"
+        height="400"
+        src="https://www.youtube.com/embed/543jwFfCZvg?si=qPcHutduGIYha_68"
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      ></iframe>
+      <div className="flex flex-col content-start w-auto p-5 gap-3">
+        <div>
+          <p className="text-base">
+            Description shdgajhd dáhjgda ádjgdjahs djasgdasjbd jasgd ádjasgdajs
+            duiassgdasj
+          </p>
+        </div>
+        <div className="flex flex-row-reverse w-full">
+          <CiShare1 className=" w-6 h-6" />
+        </div>
+        <div className="flex flex-row justify-between">
+          <a href="#">Details</a>
+          <a href="#">Details</a>
+          <a href="#">Details</a>
+        </div>
+        <div className="flex flex-col gap-4">
+          <div className="flex justify-between">
+            <span>Project:</span>
+            <span>100%</span>
           </div>
-          <div className="flex flex-row-reverse w-full">
-            <CiShare1 className=" w-6 h-6" />
-          </div>
-          <div className="flex flex-row justify-between">
-            <a href="#">Details</a>
-            <a href="#">Details</a>
-            <a href="#">Details</a>
-          </div>
-          <div className="flex flex-col gap-4">
-            <div className="flex justify-between">
-              <span>Project:</span>
-              <span>100%</span>
-            </div>
-            <div className="w-full bg-gray-200 rounded-full h-2.5">
-              <div
-                className="bg-green-500 h-2.5 rounded-full"
-                style={{ width: "100%" }}
-              />
-            </div>
+          <div className="w-full bg-gray-200 rounded-full h-2.5">
+            <div
+              className="bg-green-500 h-2.5 rounded-full"
+              style={{ width: "100%" }}
+            />
           </div>
         </div>
       </div>
--- components/Completed.tsx
+++ components/Completed.tsx
@@ -8,12 +8,12 @@ const Completed = () => {
         Completed Projects
       </h1>
       <hr className="max-w-4xl h-0.5 mx-auto my-4 bg-xanh-le border-0 rounded md:my-10"></hr>
-      <div className="flex flex-row gap-2 mx-3">
-        <div className="max-w-3xl w-full">
+      <div className="flex flex-row justify-center gap-3 mx-3">
+        <div className="max-w-xl w-full h-full shadow-lg border border-gray-200 bg-white rounded-lg">
           <CardCompleted />
         </div>
 
-        <div className="flex flex-col gap-4 shadow-lg border border-gray-200 bg-xanh-dam rounded-lg p-5 w-full max-w-lg">
+        <div className="flex flex-col gap-4 p-5 max-w-lg w-full shadow-lg border border-gray-200 bg-xanh-dam rounded-lg">
           <span className="text-xl font-bold text-white">More</span>
           <CardMore />
           <CardMore />
